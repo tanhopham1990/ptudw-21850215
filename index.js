499833e7a2d76d5371feb46a64bc8446a044f677
TANHO <tanho@TANHOs-MacBook-Pro.local>
2023-01-22 23:51:58 +0700

    Chuyển Free HTML Template sang Express Handlebars Template

--- index.js
+++ index.js
@@ -3,9 +3,27 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const expressHandlebars = require('express-handlebars');
 
 // Cau hinh public static folder
 app.use(express.static(__dirname + '/public'));
+
+// Cau hinh su dung express handlebars
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defautlLayout: 'layout'
+}));
+app.set('view engine', 'hbs');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index');
+})
+app.get('/:page', (req, res) => {
+    res.render(req.params.page);
+})
 
 // Khoi dong web server
 app.listen(port, () => {
