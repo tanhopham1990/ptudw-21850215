d5f6c28a82c7d962f992d30d3f2178b12419ec40
Tan Ho <tanhopham1990@gmail.com>
2023-01-30 22:35:08 +0700

    mô hình MVC

--- index.js
+++ index.js
@@ -18,18 +18,7 @@ app.engine('hbs', expressHandlebars.engine({
 app.set('view engine', 'hbs');
 
 // Routes
-app.get('/createTables', (req, res) => {
-    let models = require('./models');
-    models.sequelize.sync().then(() => {
-        res.send('Table created!')
-    });
-});
-app.get('/', (req, res) => {
-    res.render('index');
-})
-app.get('/:page', (req, res) => {
-    res.render(req.params.page);
-})
+app.use('/', require('./routes/indexRouter'));
 
 // Khoi dong web server
 app.listen(port, () => {
